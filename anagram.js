1fc8438d44e02fe3a2c50444987e56c18656b3f1
Redfish <lesonduko@gmail.com>
2016-07-13 11:14:53 +0200

    debut de resolution exemple d'erwann

--- anagram.js
+++ anagram.js
@@ -10,8 +10,33 @@ function Anagram(word) {
   this.word = word;
 }
 
-Anagram.prototype.matches = function (words) {
-//
-// YOUR CODE GOES HERE
-//
+Anagram.prototype.generateComparator(mot){
+	var a = mot.split("").sort();
+	return a.join().toUpperCase();
 }
+
+Anagram.prototype.matches = function (words) {
+
+	var Tab = [];/***test jasmin operationelle*****/
+	var wordCompare = this.generateComparator(this.word);
+	
+		for(var i = 0;i>word.length;;i++){
+			var mot= words[i];
+			var motCompare = this.generateComparator(mot)
+			
+			if(mot.length==this.word.length){
+				tab.push(mot);
+				if(wordCompare==motCompare){
+					Tab.push(mot);
+			}
+		}
+		return Tab;		
+
+		}
+	}
+
+
+   
+
+    
+
